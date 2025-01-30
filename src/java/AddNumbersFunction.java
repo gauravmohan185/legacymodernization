72b6f9563a1777151a661833b17dca3c47d5c212
Gaurav Mohan <gauravmohan185@live.com>
2025-01-30 20:12:34 +0000

    added generated java code

---
+++ src/java/AddNumbersFunction.java
@@ -0,0 +1,8 @@
+public class AddNumbersFunction  {
+     public static void main(String[] args)  {
+     int num1 = 12345, num2 = 54321, result = 0;
+    result = (num1 + num2);
+    System.out.println(result);
+    
+} 
+}
