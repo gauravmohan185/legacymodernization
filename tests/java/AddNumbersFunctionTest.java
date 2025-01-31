575405ca7985ed49cb45fd996d531894b21c6183
Gaurav Mohan <gauravmohan185@live.com>
2025-01-31 11:18:50 +0000

    added generated java test cases

---
+++ tests/java/AddNumbersFunctionTest.java
@@ -0,0 +1,7 @@
+public class AddNumbersFunctionTest  {
+     public static void main(String[] args)  {
+     int num1 = 12345, num2 = 54321, expected = 66666, result = AddNumbersFunction.addFunction(num1, num2);
+    System.out.println(result == expected ? "Test Passed" : "Test Failed");
+    
+} 
+}
