5cd5b5ca691411af92f4eedc032a8ba26103987c
Gaurav Mohan <gauravmohan185@live.com>
2025-01-31 08:06:02 +0000

    added generated java test cases

---
+++ tests/java/AddNumbersFunctionTest.java
@@ -0,0 +1,10 @@
+public class AddNumbersFunction  {
+     public static void main(String[] args)  {
+     int NUM1 = 12345, NUM2 = 54321;
+    System.out.println(addFunction(NUM1, NUM2));
+    
+} static int addFunction(int param1, int param2)  {
+     return param1 + param2;
+    
+} 
+}
